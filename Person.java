3fe3e253e49ffd03fbf48a34ebca7b9aab3ab031
tanmaytejra456 <tanmaytejra456@gmail.com>
2026-09-17 23:55:41 +0530

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    protected String name;
+    protected String phone;
+
+    public Person(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
